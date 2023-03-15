35f80cd36ec68e6f8222d4949487d050388b3312
Enrique Ramirez <enraher@gmail.com>
2023-03-15 19:15:24 +0100

    Project dependences

---
+++ Pods/Target Support Files/Pods-RickAndMorty-IOS-VIPER-RickAndMorty-IOS-VIPERUITests/Pods-RickAndMorty-IOS-VIPER-RickAndMorty-IOS-VIPERUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RickAndMorty_IOS_VIPER_RickAndMorty_IOS_VIPERUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RickAndMorty_IOS_VIPER_RickAndMorty_IOS_VIPERUITests
+@end
---
+++ Pods/Target Support Files/Pods-RickAndMorty-IOS-VIPER/Pods-RickAndMorty-IOS-VIPER-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RickAndMorty_IOS_VIPER : NSObject
+@end
+@implementation PodsDummy_Pods_RickAndMorty_IOS_VIPER
+@end
